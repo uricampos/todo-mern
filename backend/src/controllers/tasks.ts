4722e6f35857d352848ffcedc4903e632151259f
Uriel Campos <urieldecampos@gmail.com>
2023-11-02 11:20:47 -0300

    Creating http erros for bad request, internal server error, etc

--- backend/src/controllers/tasks.ts
+++ backend/src/controllers/tasks.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from 'express';
 import TaskModel from '../models/task';
+import createHttpError from 'http-errors';
+import mongoose from 'mongoose';
 
 export const getTasks: RequestHandler = async (req, res, next) => {
     try {
@@ -10,22 +12,45 @@ export const getTasks: RequestHandler = async (req, res, next) => {
     }
 };
 
-export const getTask: RequestHandler = async(req, res, next) => {
+export const getTask: RequestHandler = async (req, res, next) => {
     const taskId = req.params.taskId;
-    
+
     try {
+        if (!mongoose.isValidObjectId(taskId)) {
+            throw createHttpError(400, 'Invalid task id');
+        }
+
         const task = await TaskModel.findById(taskId).exec();
+
+        if (!task) {
+            throw createHttpError(404, 'Task not found');
+        }
+
         res.status(200).json(task);
     } catch (error) {
         next(error);
     }
+};
+
+interface CreateTaskBody {
+    title?: string;
+    text?: string;
 }
 
-export const createTask: RequestHandler = async (req, res, next) => {
+export const createTask: RequestHandler<
+    unknown,
+    unknown,
+    CreateTaskBody,
+    unknown
+> = async (req, res, next) => {
     const title = req.body.title;
     const text = req.body.text;
 
     try {
+        if (!title) {
+            throw createHttpError(400, 'Task must have a title');
+        }
+
         const newTask = await TaskModel.create({
             title: title,
             text: text,
