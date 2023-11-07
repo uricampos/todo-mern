f0952001124b00b4cda19cd396bd97d5a9daebb0
Uriel Campos <urieldecampos@gmail.com>
2023-11-07 17:34:12 -0300

    Updating and deleting tasks from frontend

--- frontend/src/network/tasks_api.ts
+++ frontend/src/network/tasks_api.ts
@@ -19,14 +19,32 @@ export async function fetchTasks(): Promise<Task[]> {
 }
 
 export interface TaskInput {
-    title: string,
-    text?: string,
-    completed: boolean,
+    title: string;
+    text?: string;
+    completed: boolean;
 }
 
 export async function createTask(task: TaskInput): Promise<Task> {
     const response = await fetchData('/api/tasks', {
         method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(task),
+    });
+    return response.json();
+}
+
+export async function deleteTask(taskId: string) {
+    await fetchData('/api/tasks/' + taskId, { method: 'DELETE' });
+}
+
+export async function updateTask(
+    taskId: string,
+    task: TaskInput
+): Promise<Task> {
+    const response = await fetchData('/api/tasks/' + taskId, {
+        method: 'PATCH',
         headers: {
             'Content-Type': 'application/json',
         },
